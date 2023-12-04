46f3a27c46979d60dff5e8af9bec2cb16db181e8
EasyUM-8 <natalia.devpro@icloud.com>
2023-12-04 20:26:32 +0300

    исправлен баг при регистрации

--- src/dz/Main.java
+++ src/dz/Main.java
@@ -2,8 +2,9 @@ package dz;
 
 public class Main{
     public static void main(String[] args) {
-        Hen hen = HenFactory.getHеn("Беларусь"); // проявление пораждающего шаблона Factory
+        Hen hen = HenFactory.getHеn("Беларусь!!!"); // проявление пораждающего шаблона Factory
         hen.getCountOfEggsPerMonth();
         System.out.print(hen.getDescription());
+        System.out.println();
     }
 }
